51dd9e72e40f98661fd560b78d453f386bc57961
Lauric-h <h.lauric@outlook.com>
2021-10-25 11:37:29 +0200

    call to /api/vehicule/type=type working

--- src/main/java/com/springmsa/deathstar/controller/vehicles/VehicleController.java
+++ src/main/java/com/springmsa/deathstar/controller/vehicles/VehicleController.java
@@ -1,12 +1,17 @@
 package com.springmsa.deathstar.controller.vehicles;
 
+import com.springmsa.deathstar.model.Vehicle;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -14,13 +19,28 @@ public class VehicleController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${VEHICLE_SERVER}")
+    private String VEHICLE_SERVER;
+
     // post /api/bookings/vehicles -> calls vehicles
     @PostMapping(value = "/api/bookings/vehicles")
-    public void fetchAvailableVehicles(@RequestBody Map<String, Object> rq) {
-        System.out.println(rq.get("start"));
+    public ResponseEntity<Vehicle[]> fetchAvailableVehicles(@RequestBody Map<String, Object> rq) {
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                .scheme("http").host(VEHICLE_SERVER).path("/type").build();
+
+        String urlTemplate = UriComponentsBuilder.fromHttpUrl(uriComponents.toUriString())
+                .queryParam("type", rq.get("type"))
+                .encode()
+                .toUriString();
+
+        return restTemplate.getForEntity(urlTemplate, Vehicle[].class);
     }
 
     // post /api/bookings/book -> calls vehicles
+    @PostMapping(value = "/api/bookings/book")
+    public void fetchAvailableVehicleById(@RequestBody Map<String, Object> rq) {
+        System.out.println(rq);
+    }
 
     // post /api/bookings/confirmation -> calls clients
 }
